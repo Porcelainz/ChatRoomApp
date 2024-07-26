cde19f8d4d138293bfe091dd700168adb58a2f77
Porcelain_ <ai.red.porcelain@gmail.com>
2024-07-26 16:37:03 +0800

    current work fine that client can parse more than 1000 length message

--- Test.Client/Program.cs
+++ Test.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -72,20 +73,41 @@ namespace Test.Client
 			{
 				while (true)
 				{
-					// 讀取消息長度
-					//var lengthBuffer = new byte[4];
-					int bytesRead = await _stream.ReadAsync(lengthBuffer, 0, lengthBuffer.Length);
-					if (bytesRead == 0) break; // 連接已關閉
-					var messageLength = BitConverter.ToInt32(lengthBuffer, 0);
-					Array.Clear(messageBuffer, 0, messageBuffer.Length);
-					// 根據消息長度讀取消息
-					//var messageBuffer = new byte[messageLength];
-					bytesRead = await _stream.ReadAsync(messageBuffer, 0, messageLength);
-					if (bytesRead == 0) break; // 連接已關閉
-					var message = Encoding.UTF8.GetString(messageBuffer, 0, bytesRead);
+					//// 讀取消息長度
+					////var lengthBuffer = new byte[4];
+					//int bytesRead = await _stream.ReadAsync(lengthBuffer, 0, lengthBuffer.Length);
+					//if (bytesRead == 0) break; // 連接已關閉
+					//var messageLength = BitConverter.ToInt32(lengthBuffer, 0);
+					//Array.Clear(messageBuffer, 0, messageBuffer.Length);
+					//// 根據消息長度讀取消息
+					////var messageBuffer = new byte[messageLength];
+					//bytesRead = await _stream.ReadAsync(messageBuffer, 0, messageLength);
+					//if (bytesRead == 0) break; // 連接已關閉
+					//var message = Encoding.UTF8.GetString(messageBuffer, 0, bytesRead);
+					int bytesRead = await _stream.ReadAsync(messageBuffer, 0, 4);
+					if (bytesRead == 0) break;
+					var messageLength = BitConverter.ToInt32(messageBuffer, 0);
 
-					Console.WriteLine(message);
-					
+					// 使用 MemoryStream 來構建完整消息
+					using (var memoryStream = new MemoryStream())
+					{
+						int remainingBytes = messageLength;
+						while (remainingBytes > 0)
+						{
+							int bytesToRead = Math.Min(remainingBytes, messageBuffer.Length);
+							bytesRead = await _stream.ReadAsync(messageBuffer, 0, bytesToRead);
+							if (bytesRead == 0) break;
+							await memoryStream.WriteAsync(messageBuffer, 0, bytesRead);
+							remainingBytes -= bytesRead;
+						}
+
+						var messageBytes = memoryStream.ToArray();
+						var message = Encoding.UTF8.GetString(messageBytes);
+
+
+						Console.WriteLine(message);
+
+					}
 				}
 			}
 			catch (Exception ex)
